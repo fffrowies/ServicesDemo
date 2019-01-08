8daae0ed67bb6ed7495f51dacbda50331a254cfe
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-08 13:12:04 -0300

    Explore started service

--- app/src/main/java/com/fffrowies/servicesdemo/MainActivity.java
+++ app/src/main/java/com/fffrowies/servicesdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.fffrowies.servicesdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startStartedService(View view) {
+
+        Intent intent = new Intent(MainActivity.this, MyStartedService.class);
+        startService(intent);
+    }
+
+    public void stopStartedService(View view) {
+
+        Intent intent = new Intent(MainActivity.this, MyStartedService.class);
+        stopService(intent);
     }
 }
